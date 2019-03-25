fdd6074fee024368d1f6701522a0c57a10a3a575
Amir Mofakhar <amir@mofakhar.info>
2019-03-25 17:37:19 +0100

    update celery runner

--- run_celery_worker.sh
+++ run_celery_worker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-celery worker --app=payments.fetch_data.worker.celery_app --concurrency=1 --loglevel=INFO
+celery worker --app=payments.fetch_data.tasks --concurrency=1 --loglevel=INFO -B
