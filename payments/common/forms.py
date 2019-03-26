ec3c92ec57286e9c6dd5e95c5f3b5d83fdb6ca49
Amir Mofakhar <amir@mofakhar.info>
2019-03-26 23:44:43 +0100

    updated requirements

---
+++ payments/common/forms.py
@@ -0,0 +1,17 @@
+"""
+By Amir Mofakhar <amir@mofakhar.info>
+"""
+from wtforms import StringField, Form, SelectField, SubmitField, HiddenField
+
+from payments.fetch_data.tasks import get_fields_from_celery
+
+
+class SearchForm(Form):
+
+    fields = get_fields_from_celery()
+
+    fields.insert(0, ('', 'Select a field ...'))
+    search_field = SelectField(u'Searching field', choices=fields, id='search_field', )
+    search_data = StringField('', id='search_data_complete', render_kw={'class': 'search_box'})
+    field_to_search = HiddenField(id='field_to_search')
+    submit_search = SubmitField('Search')
---
+++ payments/endpoints/download.py
@@ -0,0 +1,38 @@
+"""
+By Amir Mofakhar <amir@mofakhar.info>
+"""
+from flask import Blueprint, request
+import flask_excel
+
+from payments.fetch_data.tasks import get_data_from_celery, get_fields_from_celery
+
+from payments.common.utils import get_data_from_dict
+
+
+output_excel = flask_excel
+
+
+bp = Blueprint('download', __name__)
+
+
+@bp.route('/download_excel', methods=['GET'])
+def download_excel():
+
+    field = request.args.get('field')
+    keyword = request.args.get('keyword')
+    excel_list = []
+    titles = []
+    raw_title = []
+    for raw_column, column in get_fields_from_celery():
+        titles.append(column)
+        raw_title.append(raw_column)
+
+    data = get_data_from_dict(get_data_from_celery(), field, keyword)
+    excel_list.append(titles)
+    for record in data:
+        record_list = []
+        for field in raw_title:
+            record_list.append(record.get(field, ''))
+        excel_list.append(record_list)
+    return_file = output_excel.make_response_from_array(excel_list, 'xls', file_name='payments')
+    return return_file
